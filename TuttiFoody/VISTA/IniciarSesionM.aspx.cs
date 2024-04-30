a8cc648fccb91713132b2ceac8eb7a9b3d775032
AndreiMarinCatalin <andrei1931311@gmail.com>
2024-04-30 11:20:16 +0200

    creando pagina con maestra para el popup

---
+++ TuttiFoody/VISTA/IniciarSesionM.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TuttiFoody.DAL;
+
+namespace TuttiFoody.VISTA
+{
+    public partial class IniciarSesionM : Page
+    {
+        UsuarioDAL usuarioDAL = new UsuarioDAL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnIniciaSesion_OnClick(object sender, EventArgs e)
+        {
+            comprobarCredenciales();
+        }
+
+        private void comprobarCredenciales()
+        {
+            Usuario usuario = usuarioDAL.SelectByName(txtName.Value);
+            if (usuario != null)
+            {
+                if (usuario.Contraseña == txtPassword.Value)
+                {
+                    errorMsg.InnerText = "BIEEEEEN";
+
+                }
+                else
+                {
+                    errorMsg.InnerText = "Credenciales incorrectas.";
+
+                }
+            }
+            else
+            {
+                errorMsg.InnerText = "Usuario inexistente.";
+            }
+        }
+    }
+}
